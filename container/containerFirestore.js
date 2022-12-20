105777ebe48f34f3fb4d8de4efb1a8fc732451dc
Martin Cueva <cuevabros@hotmail.com>
2022-12-20 17:43:54 -0300

    Trabajo Final Segunda Entrega con Id base de datos Firebase Personalizado

---
+++ container/containerFirestore.js
@@ -0,0 +1,189 @@
+import { firestoreDatabase } from '../db/firestoreClient.js';
+
+class Container{
+
+    coleccion;
+
+    constructor(nombreColeccion) {
+        this.coleccion = firestoreDatabase.collection(nombreColeccion)    
+    }
+
+
+
+    //PRODUCTOS y CARRITO
+    async save(objeto){
+
+        try {
+            const ref = await this.coleccion.add(objeto)
+            return { ...objeto, _id: ref.id }
+        } 
+        catch (error){
+            error => { throw error}
+        } 
+
+      }
+
+
+          //PRODUCTOS y CARRITO
+    async nombreDocumento(id){
+       
+        try {
+
+            const snapshot = await this.coleccion.where("_id", "=", id).get()
+
+            if(!snapshot){
+                return null
+            }else{
+                const resultado = []
+                snapshot.forEach(doc => {
+                    resultado.push({id: doc.id, ...doc.data() })
+                })
+                return resultado[0].id;
+            }
+            
+        }
+
+        catch(error){
+            error => { throw error}
+        } 
+
+     }
+
+    //PRODUCTOS y CARRITO
+    async getById(id){
+       
+        try {
+
+            const snapshot = await this.coleccion.where("_id", "=", id).get()
+
+            if(!snapshot){
+                return null
+            }else{
+                const resultado = []
+                snapshot.forEach(doc => {
+                    resultado.push({_id: doc.id, ...doc.data() })
+                })
+                return resultado;
+            }
+            
+        }
+
+        catch(error){
+            error => { throw error}
+        } 
+
+     }
+
+
+    //PRODUCTOS y CARRITO
+     async getAll(){
+
+        try {
+
+            const snapshot = await this.coleccion.get()
+
+            if(!snapshot){
+                return null
+            }else{
+
+                const resultado = []
+
+                snapshot.forEach(doc => {
+                    resultado.push(doc.id, { ...doc.data() })
+                })
+
+                return resultado;
+            }
+            
+        }
+
+        catch(error){
+            error => { throw error}
+        } 
+
+    }
+
+
+    //PRODUCTOS
+    async deleteById(id){
+        try {
+            const objetoBorrado = await this.getById(id)
+            const id_documento = await this.nombreDocumento(id)
+
+         if(objetoBorrado[0]){ 
+            await this.coleccion.doc(id_documento).delete()
+            return objetoBorrado[0]
+          } else {
+            return null
+          }
+        }
+
+        catch(error){
+            error => { throw error}
+        } 
+
+    }
+
+    
+    //PRODUCTOS y CARRITO
+    async update(objeto){
+
+        try {
+            const id_documento = await this.nombreDocumento(objeto._id)
+            await this.coleccion.doc(id_documento).update({name: objeto.name, description: objeto.description, price: objeto.price, imageurl: objeto.imageurl})
+            return objeto;
+        }
+       catch(error){
+            error => { throw error}
+        } 
+    }
+
+    
+    //CARRITO
+    async save_products(objeto){
+
+        try {
+            const id_documento = await this.nombreDocumento(objeto._id)
+            await this.coleccion.doc(id_documento).update({productos: objeto.productos})
+            return objeto;
+        }
+        catch(error){
+            error => { throw error }
+        } 
+    }
+
+
+    //CARRITO
+    async deleteByIdCart(id){
+
+        try {
+            const id_documento = await this.nombreDocumento(id)
+            const carritoVaciado = await this.coleccion.doc(id_documento).update({productos: []})
+            return carritoVaciado
+        }
+        catch(error){
+            error => { throw error}
+        } 
+    }
+
+
+    //CARRITO
+    async deleteByIdProd(indice_cart, indice_prod){
+
+        try {
+            this.cart = await this.getAll()
+            const id_documento = await this.nombreDocumento(this.cart[indice_cart]._id)
+            const eliminado = this.cart[indice_cart].productos.splice(indice_prod, 1)
+            await this.coleccion.doc(id_documento).update({productos: this.cart[indice_cart]})
+            return eliminado
+        }
+        catch(error){
+            error => { throw error}
+        } 
+    }
+
+
+ }
+
+
+ export default Container;
---
+++ controllers/controllerShoppingCart.js
@@ -0,0 +1,160 @@
+import {randomUUID}  from 'crypto';
+import {PERSISTENCIA}  from '../db/config.js'
+import {user}  from '../db/config.js'
+import ContainerMongodb from '../container/containerMongodb.js'
+import ContainerFirestore from '../container/containerFirestore.js'
+import Container from '../container/containerFs.js'
+import ContainerBDRelacionalProd from '../container/containerBDRelacionalProd.js'
+import ContainerBDRelacionalCart from '../container/containerBDRelacionalCart.js'
+import { clienteSql } from '../db/clienteSql.js';
+import { clienteSqlLite3 } from '../db/clienteSql.js';
+
+let prodTest
+let cartTest
+
+
+switch (PERSISTENCIA) {
+    case 'mongodb':
+        prodTest = new ContainerMongodb('productos')
+        cartTest = new ContainerMongodb('cart')
+        break
+    case 'firebase':
+        prodTest = new ContainerFirestore('productos')
+        cartTest = new ContainerFirestore('cart')
+        break
+    case 'mysql':
+        prodTest = new ContainerBDRelacionalProd(clienteSql, 'productos')
+        cartTest = new ContainerBDRelacionalCart(clienteSql, 'cart', 'cartprods', 'productos')
+        break
+    case 'sqlite':
+        prodTest = new ContainerBDRelacionalProd(clienteSqlLite3, 'productos')
+        cartTest = new ContainerBDRelacionalCart(clienteSqlLite3, 'cart', 'cartprods', 'productos')
+        break
+    case 'fs':
+        prodTest = new Container('productos.txt')
+        cartTest = new Container('cart.txt')
+}
+
+
+async function controladorPostItems(req, res) {
+    res.status(201);
+    const objeto = req.body;
+    const productos = []
+    objeto._id = randomUUID();
+    objeto.productos = productos
+
+    if(PERSISTENCIA === "mysql"  || PERSISTENCIA === "sqlite")
+     await cartTest.save({_id: objeto._id, user: user});
+    else
+     await cartTest.save(objeto);
+
+    res.json({id: objeto._id})
+}
+
+async function controladorPostItemProducts({ body, params: { id_cart } }, res) {
+    const Items = await cartTest.getAll();
+    const Prods = await prodTest.getAll();
+
+    const indiceProducto = Prods.findIndex(p => p._id === body._id);
+
+    if (indiceProducto === -1) {
+        res.status(404);
+        res.json({ mensaje: `no se encontró producto con ese id (${body._id})` });
+    } else {
+        body = Prods[indiceProducto]
+
+
+        const indiceBuscado = Items.findIndex(c => c._id === id_cart);
+
+
+        if (indiceBuscado === -1) {
+            res.status(404);
+            res.json({ mensaje: `no se encontró carrito con ese id (${id_cart})` });
+        } else {
+          if(PERSISTENCIA === "mysql"  || PERSISTENCIA === "sqlite"){
+            const objeto = {_idCart: id_cart, _idPRod: body._id }
+            const insertado = await cartTest.save_products(objeto);
+            res.json(insertado)
+          }
+          else {
+            Items[indiceBuscado].productos.push(body);
+            await cartTest.save_products(Items[indiceBuscado]);
+            res.json(body);
+          }
+
+        }
+
+    }
+}
+
+
+async function controladorGetItemsSegunId({ params: { id_cart } }, res) {
+    const Items = await cartTest.getById(id_cart);
+
+    if (!Items) {
+        res.status(404);
+        res.json({ mensaje: `no se encontró carrito con ese id (${id_cart})` });
+    } else {
+        res.json(Items);
+    }
+}
+
+
+
+async function controladorDeleteItemsSegunId({ params: { id_cart } }, res) {
+    const Items = await cartTest.getAll();
+    const indiceBuscado = Items.findIndex(c => c._id === id_cart);
+    const borrados = Items[indiceBuscado]
+
+    if (indiceBuscado === -1) {
+        res.status(404);
+        res.json({ mensaje: `no se encontró carrito con ese id (${id_cart})` });
+    } else {
+        await cartTest.deleteByIdCart(id_cart);
+        res.json(borrados);
+    }
+}
+
+async function controladorDeleteItemsSegunIdProducts({ params: { id_cart }, params: { id_prod } }, res) {
+    const Items = await cartTest.getAll();
+    const Prods = await prodTest.getAll();
+
+
+    const indiceBuscadoCart = Items.findIndex(c => c._id === id_cart);
+
+    let indiceBuscadoProd
+    if(PERSISTENCIA === "mysql"  || PERSISTENCIA === "sqlite"){
+         indiceBuscadoProd = Prods.findIndex(p => p._id === id_prod);
+      }
+    else { 
+         indiceBuscadoProd = Items[indiceBuscadoCart].productos.findIndex(p => p._id === id_prod);
+    }
+
+    if (indiceBuscadoCart === -1) {
+        res.status(404);
+        res.json({ mensaje: `no se encontró carrito con ese id (${id_cart})` });
+
+        if (indiceBuscadoProd === -1) {
+            res.json({ mensaje: `no se encontró producto con ese id (${id_prod})` });        
+        }    
+
+    } else {
+      if (indiceBuscadoProd === -1) {
+        res.status(404);
+        res.json({ mensaje: `no se encontró producto con ese id (${id_prod}), en el carrito con id (${id_cart}` });
+      } else {
+        let borrados
+        if(PERSISTENCIA === "mysql"  || PERSISTENCIA === "sqlite")
+             borrados = await cartTest.deleteByIdProd(id_cart, id_prod);
+        else
+             borrados = await cartTest.deleteByIdProd(indiceBuscadoCart, indiceBuscadoProd);
+        res.json(borrados);
+      }
+    }
+
+}
+
+
+
+export  {controladorPostItems, controladorPostItemProducts, controladorGetItemsSegunId,
+controladorDeleteItemsSegunId, controladorDeleteItemsSegunIdProducts};
